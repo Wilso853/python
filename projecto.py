a0924581c708d26ef0423857cf9b07fad12b8348
Wilson <wilsonfigueiredo751@mail.com>
2026-05-28 09:36:32 -0300

    novos dados

--- projecto.py
+++ projecto.py
@@ -1,1 +1,13 @@
-print("Ola mundo")
+dados = {
+    "nome":"wilson",
+    "sobrenome":"Figueiredo",
+    "idade":17,
+    "sair":""
+}
+while True:
+    msg = input("Voce: ").strip().lower()
+    resposta = dados.get(msg, "Erro, comando nao encotrado")
+    if msg == "sair":
+        print("saindo....")
+        break
+    print("Bot:", resposta)
