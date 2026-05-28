c58ce929eea14f2b5283ec2900fa41e674233aee
Wilson <wilsonfigueiredo751@mail.com>
2026-05-28 09:59:30 -0300

    calculadora adicionada

--- projecto.py
+++ projecto.py
@@ -2,12 +2,35 @@ dados = {
     "nome":"wilson",
     "sobrenome":"Figueiredo",
     "idade":17,
-    "sair":""
+    "sair":"",
+    "calculadora":""
 }
+def calculadora():
+    numero1 = int(input("digite o primeiro numero:"))
+    numero2 = int(input("digite o segundo numero:"))
+    print("(1) somar")
+    print("(2) subtrair")
+    print("(3) multiplicar")
+    print("(4) dividir")
+    opcao = input("digite um opcao:")
+    if opcao == "1":
+        print("soma:", numero1 + numero2)
+    elif opcao == "2":
+        print("subtracao:", numero1 - numero2)
+    elif opcao == "3":
+        print("multiplicacao:", numero1 * numero2)
+    elif  opcao == "4":
+        print("divisao:", numero1 / numero2)
+    else:
+        print("opcao invalida")
+
+
 while True:
     msg = input("Voce: ").strip().lower()
     resposta = dados.get(msg, "Erro, comando nao encotrado")
     if msg == "sair":
         print("saindo....")
         break
+    elif msg == "calculadora":
+        calculadora()
     print("Bot:", resposta)
